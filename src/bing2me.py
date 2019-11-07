942e75bc6e7e8532316c564b16189b2f346aa1e6
ak47 <akuma@ncwtf.com>
2019-11-07 16:43:58 +0800

    first commit

--- src/bing2me.py
+++ src/bing2me.py
@@ -9,7 +9,7 @@ import win32gui
 import win32con
 import win32api
 
-WEB_SITE = 'https://cn.bing.com'
+WEB_SITE = 'https://cn.bing:.com'
 URL_PARAM = "/?scope=web&FORM=HDRSC1"
 CONTENT_STR = "data-ultra-definition-src=\""
 BING_PIC_DIR = os.path.abspath('.') + "/bing-pic/"
